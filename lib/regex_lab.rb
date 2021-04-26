d3e32b607e67e456347b4ccf56e708183dbe4545
Codi Kraus <codi.kraus@gmail.com>
2021-04-26 19:13:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,8 @@
 def starts_with_a_vowel?(word)
-  word.downcase.match(/^[aeiou].*/)
-  return true
+  if word.downcase.match(/^[aeiou].*/)
+    return true
+  else
+    return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
